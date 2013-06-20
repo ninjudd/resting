9c670fe8b59f9bc4ade15e70582b9c5ad5f3baaa
Justin Balthrop <git@justinbalthrop.com>
2013-06-20 13:16:38 -0700

    0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.flatland/resting "0.1.0-SNAPSHOT"
+(defproject org.flatland/resting "0.1.0"
   :description "Simple ref-backed restful resources."
   :url "https://github.com/flatland/resting"
   :license {:name "Eclipse Public License"
