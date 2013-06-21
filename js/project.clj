5353ccb94cf706fc7836fda4ecbd7d6cac2bcc59
Justin Balthrop <git@justinbalthrop.com>
2013-06-21 11:09:17 -0700

    add resting.js

---
+++ js/project.clj
@@ -0,0 +1,5 @@
+(defproject org.flatland/resting-js "0.1.1"
+  :description "Javascript client for resting."
+  :url "https://github.com/flatland/resting"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"})
