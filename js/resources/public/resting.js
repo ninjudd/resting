93ff57ad80a344fd757e11f739e1d48c0a8916af
Justin Balthrop <git@justinbalthrop.com>
2013-07-05 10:44:37 -0700

    require.js

--- js/resources/public/resting.js
+++ js/resources/public/resting.js
@@ -1,67 +1,71 @@
-var Resting = function (subclass, opts) {
-  if (this instanceof Resting) return;
+define(["underscore", "jquery"], function(_, $) {
 
-  _.extend(subclass.prototype, new Resting(), opts);
-  _.extend(subclass, Resting, opts);
-};
+  var Resting = function (subclass, opts) {
+    if (this instanceof Resting) return;
+    _.extend(subclass.prototype, new Resting(), opts);
+    _.extend(subclass, Resting, opts);
+  };
 
-Resting.http = function(method, data, id) {
-  return $.ajax({
-    url: id ? this.baseUrl + '/' + encodeURIComponent(id) : this.baseUrl,
-    dataType: 'json',
-    contentType: 'application/json',
-    data: JSON.stringify(data),
-    type: method.toUpperCase()
-  }).then(null, function(results) {
-    return results.responseText ? JSON.parse(results.responseText) : {};
-  });
-};
+  Resting.http = function(method, data, id) {
+    return $.ajax({
+      url: id ? this.baseUrl + '/' + encodeURIComponent(id) : this.baseUrl,
+      dataType: 'json',
+      contentType: 'application/json',
+      data: JSON.stringify(data),
+      type: method.toUpperCase()
+    }).then(null, function(results) {
+      return results.responseText ? JSON.parse(results.responseText) : {};
+    });
+  };
 
-Resting.prototype.http = function(method, data) {
-  return this.constructor.http(method, data, this.id);
-};
+  Resting.prototype.http = function(method, data) {
+    return this.constructor.http(method, data, this.id);
+  };
 
-Resting.prototype.save = function(opts) {
-  if (this.id) {
+  Resting.prototype.save = function(opts) {
+    if (this.id) {
+      var self = this;
+      var data = _.extend(this.attrs, {force: opts.force});
+      return this.http('put', data).done(function(results) {
+        self.attrs.hash = results.hash;
+      });
+    }
+  };
+
+  Resting.prototype.rename = function(id) {
     var self = this;
-    var data = _.extend(this.attrs, {force: opts.force});
-    return this.http('put', data).done(function(results) {
-      self.attrs.hash = results.hash;
+    return this.http('patch', {id: id}).done(function() {
+      self.id = id;
     });
-  }
-};
+  };
 
-Resting.prototype.rename = function(id) {
-  var self = this;
-  return this.http('patch', {id: id}).done(function() {
-    self.id = id;
-  });
-};
+  Resting.prototype.destroy = function(opts) {
+    var self = this;
+    return this.http('delete', opts);
+  };
 
-Resting.prototype.destroy = function(opts) {
-  var self = this;
-  return this.http('delete', opts);
-};
+  Resting.load = function(id, overrides) {
+    var subclass = this;
+    if (id) {
+      return this.http('get', null, id).then(function (results) {
+        if (results) {
+          var instance = new subclass();
+          instance.id    = id;
+          instance.attrs = _.extend(results, overrides);
+          _.bindAll(instance);
+          return instance;
+        }
+      });
+    } else {
+      return jQuery.Deferred(function (promise) {
+        promise.resolve(new subclass());
+      });
+    }
+  };
 
-Resting.load = function(id, overrides) {
-  var subclass = this;
-  if (id) {
-    return this.http('get', null, id).then(function (results) {
-      if (results) {
-        var instance = new subclass();
-        instance.id    = id;
-        instance.attrs = _.extend(results, overrides);
-        _.bindAll(instance);
-        return instance;
-      }
-    });
-  } else {
-    return jQuery.Deferred(function (promise) {
-      promise.resolve(new subclass());
-    });
-  }
-};
+  Resting.list = function() {
+    return this.http('get');
+  };
 
-Resting.list = function() {
-  return this.http('get');
-};
+  return Resting;
+});
