338978d51e0a073c9673523ae044d67aff0b6ac3
Justin Balthrop <git@justinbalthrop.com>
2013-09-04 10:09:28 -0700

    0.1.9

--- js/project.clj
+++ js/project.clj
@@ -1,4 +1,4 @@
-(defproject org.flatland/resting-js "0.1.8"
+(defproject org.flatland/resting-js "0.1.9"
   :description "Javascript client for resting."
   :url "https://github.com/flatland/resting"
   :license {:name "Eclipse Public License"
